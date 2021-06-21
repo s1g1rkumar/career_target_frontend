48ded08c300a737d4d11f8126fdb788a4f56ec98
Deepak <deepakchandrawanshi1063@gmail.com>
2021-06-21 13:13:32 +0530

    profile updated

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
 import "../CSS/Footer.css";
 import { NavLink } from "react-router-dom";
 import "../CSS/Footer.css";
+import validator from "validator";
 
 function Footer() {
+  const subs_mail = useRef(null);
+  const history = useHistory();
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  // const submitForm = (e) => {
+  //   e.preventDefault();
+  //   const emailID = subs_mail.current.value;
+  //   emailID === "" ? alert("please pass an email") : setShow(true);
+  //   if (show) {
+  //     setEmail(emailID);
+  //   }
+  // };
+  const handleInputs = (e) => {
+    setEmail(e.target.value);
+  };
+  console.log("eamil is ", email);
+  const sendMail = async (e) => {
+    const ismail = validator.isEmail(email);
+    const subs = document.getElementById("subs_mail");
+    if (ismail) {
+      e.preventDefault();
+      const res = await fetch("/subscribe", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: email }),
+      }); //fetch close
+
+      // const data = await res.json();
+      // console.log(data);
+      console.log(res.status);
+      if (res.status === 200) {
+        // dispatch({ type: "USER", payload: true });
+        // window.alert("welcome : " + data.message.email);
+        window.alert("subscribed sucessfully");
+        subs.innerHTML = " ";
+        history.push("/");
+      } else {
+        window.alert("unable to subscribe");
+      }
+    } else {
+      window.alert("please enter a valid emial");
+    }
+  };
+
   return (
     <>
       <div className="container-fluid pt-5 pb-3 bg-light border-top">
@@ -29,8 +75,27 @@ function Footer() {
             </div>
 
             <div className="col-md-6 col-sm-12">
-              <input type="text" placeholder="Enter Email" className="mail" />
-              <button className="btn-danger ml-2 subs">Subscribe</button>
+              <form class="row g-3">
+                <div class="col-auto">
+                  <input
+                    type="text"
+                    class="form-control border rounded-pill"
+                    placeholder="Email"
+                    id="subs_mail"
+                    // ref={subs_mail}
+                    onChange={handleInputs}
+                  />
+                </div>
+                <div class="col-auto">
+                  <button
+                    type="submit"
+                    class="btn btn-danger mb-3 border rounded-pill"
+                    onClick={sendMail}
+                  >
+                    Subscribe
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
